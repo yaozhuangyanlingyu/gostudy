1f70b6a9e5afda4e09f2abad71c629dffc1b834b
yaoxf <850630887@qq.com>
2020-11-19 01:47:14 +0800

    md

---
+++ gin_study/cookie01/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/cookie", func(c *gin.Context) {
+		cookie, err := c.Cookie("gin_cookie") // 获取Cookie
+		if err != nil {
+			cookie = "NotSet"
+
+			// 设置Cookie
+			c.SetCookie("gin_cookie", "test", 3600, "/", "127.0.0.1", false, true)
+		}
+		fmt.Printf("Cookie value: %s \n", cookie)
+	})
+
+	router.Run()
+}
