dbd1c0c2eb5916161e599e39dfeb2a612050e749
yaoxf <850630887@qq.com>
2020-11-29 16:53:10 +0800

    md

--- gomicro/webapi02/main.go
+++ gomicro/webapi02/main.go
@@ -1,5 +1,19 @@
 package main
 
+import(
+	"github.com/micro/go-micro/web"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
 func main() {
-    
+	ginRouter := gin.Default()
+	ginRouter.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello world")
+	})
+	server := web.NewService(
+		web.Address(":8081"),
+		web.Handler(ginRouter),
+	)
+	server.Run()
 }
