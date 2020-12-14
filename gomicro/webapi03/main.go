1c9deadd6fb8a5c835ee7ef784b928b15b805c8b
yaoxf <850630887@qq.com>
2020-12-15 00:33:22 +0800

    md

--- gomicro/webapi03/main.go
+++ gomicro/webapi03/main.go
@@ -4,18 +4,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-micro/web"
+	"github.com/micro/go-plugins/registry/consul"
 )
 
 func main() {
+	// 将服务注册到consul
+	consulReg := consul.NewRegistry(
+		registry.Addrs("192.168.95.129:8510"),
+	)
+
+	// 使用gin路由
 	ginRouter := gin.Default()
 	ginRouter.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "Hello world")
 	})
 
+	// 启动go micro服务
 	server := web.NewService(
-		web.Address(":8081"),
-		web.Handler(ginRouter),
+		web.Name("product-service"), // 服务名称
+		web.Address(":8081"),        // 监听IP:端口
+		web.Handler(ginRouter),      // 使用gin路由
+		web.Registry(consulReg),     // 注册到consul
 	)
 	server.Run()
 }
