22d90a08b99bf0fa2b070acc9c7ebe6ecb18d558
yaoxf <850630887@qq.com>
2021-01-11 01:21:21 +0800

    md

--- cart-srv/go.mod
+++ cart-srv/go.mod
@@ -5,6 +5,6 @@ go 1.13
 require (
 	github.com/micro/go-plugins/registry/consul v0.0.0-20200119172437-4fe21aa238fd // indirect
 	github.com/spf13/viper v1.7.1 // indirect
-	github.com/yaozhuangyanlingyu/gostudy v0.0.0-20210110074853-dc0cbfb0bcac
+	github.com/yaozhuangyanlingyu/gostudy v0.0.0-20210110171157-bc288129c5af
 	google.golang.org/protobuf v1.23.0 // indirect
 )
--- cart-srv/main.go
+++ cart-srv/main.go
@@ -6,41 +6,24 @@ import (
 	"github.com/micro/go-micro"
 	"github.com/micro/go-micro/registry"
 	"github.com/micro/go-plugins/registry/consul"
-	"github.com/spf13/viper"
 	"github.com/yaozhuangyanlingyu/gostudy/cart-srv/api"
+	"github.com/yaozhuangyanlingyu/gostudy/micro-srv/loader"
 	"github.com/yaozhuangyanlingyu/gostudy/proto/cart"
 )
 
-// 读取配置文件
-func LoadConfig(path string) {
-	bindEnv()
-
-	if viper.GetString("env") == "" {
-		log.Fatal("没有指定[MICRO_SERVICE_ENV]环境变量,usage export MICRO_SERVICE_ENV=dev or usage export MICRO_SERVICE_ENV=prod")
-	}
-	log.Printf("当前运行环境 %s", viper.GetString("env"))
-
-	LoadEnvConfig(path)
-}
-
-func bindEnv() {
-	viper.SetEnvPrefix("micro_service")
-	viper.BindEnv("env") // 区分线上线下的环境变量 dev | prod
-}
-
 func main() {
-	LoadConfig("")
+	loader.LoadConfig("")
 
 	// 将服务注册到consul
 	consulReg := consul.NewRegistry(
-		registry.Addrs(Config.GetString("consul.address")),
+		registry.Addrs(loader.Config.GetString("consul.address")),
 	)
 
 	// 创建micro服务
 	service := micro.NewService(
-		micro.Name("cart-srv-test"),
+		micro.Name(loader.Config.GetString("server.name")),
 		micro.Registry(consulReg),
-		micro.Address(Config.GetString("server.address")),
+		micro.Address(loader.Config.GetString("server.address")),
 	)
 
 	// 解析命令参数
