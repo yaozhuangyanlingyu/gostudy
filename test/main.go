283b9de13181d795c6c5eccc18b0d5060a4b98e5
yaoxf <850630887@qq.com>
2021-01-24 21:50:33 +0800

    md

--- test/main.go
+++ test/main.go
@@ -4,25 +4,6 @@ import (
 	"fmt"
 )
 
-type TransInfo struct {
-	ID int32
-}
-
-type Fragment interface{
-	Exec(transInfo *TransInfo) error
-}
-
-type GetPodAction struct{}
-
-func (g GetPodAction) Exec(transInfo *TransInfo) error {
-	return nil
-}
-
 func main() {
-	var a Fragment = new(GetPodAction)
-	fmt.Println(a)
+	fmt.Println("hello")
 }
-
-
-
-
