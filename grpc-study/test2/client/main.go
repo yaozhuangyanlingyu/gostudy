dde40cfd7044a15303333e1c126d617c3cecbc97
yaoxf <850630887@qq.com>
2021-01-26 23:44:07 +0800

    md

---
+++ grpc-study/test2/client/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/yaozhuangyanlingyu/gostudy/grpc-study/test2/proto"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	// 连接服务器
+	conn, err := grpc.Dial(":8848", grpc.WithInsecure())
+	if err != nil {
+		fmt.Printf("faild to connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := proto.NewProductClient(conn)
+	res, err := c.GetProduct(context.Background(), &proto.ProductRequest{Pid: "123"})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(res.Pid, res.Status)
+}
--- grpc-study/test2/server/main.go
+++ grpc-study/test2/server/main.go
@@ -1,9 +1,36 @@
 package main
 
-import(
+import (
+	"context"
+	"fmt"
+	"net"
 
+	"github.com/yaozhuangyanlingyu/gostudy/grpc-study/test2/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-func main() {
+type server struct{}
 
+func (s *server) GetProduct(ctx context.Context, req *proto.ProductRequest) (*proto.ProductResponse, error) {
+	return &proto.ProductResponse{
+		Pid:    "100",
+		Status: "onsale",
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", "127.0.0.1:8848")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	s := grpc.NewServer()
+	proto.RegisterProductServer(s, &server{})
+	reflection.Register(s)
+	err = s.Serve(lis)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
